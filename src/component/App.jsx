adeef7fbced0b3ab4205a07fd09db217b51d6904
Mohd Muntaquim <112265229+Avenger110@users.noreply.github.com>
2024-10-20 02:15:45 +0530

    Initial commit

---
+++ src/component/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./footer";
+import Notes from "./notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ src/component/notes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1> your notes here </h1>
+      <p> Here is your notes </p>
+    </div>
+  );
+}
+export default Notes;
